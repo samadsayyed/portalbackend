3480c6cbdc333ef1e61d509793e5cb15e886a185
samadsayyed <samadsayyed812@gmail.com>
2023-06-11 12:42:03 +0530

    setting cors

--- app.js
+++ app.js
@@ -8,14 +8,18 @@ import cors from "cors"
 config({path:"./config/config.env"})
 
 export const app = express()
-app.use(cors({
-  origin: process.env.Frontend, // Replace with your allowed origin(s),
-  credentials: true,
-}));
 app.use(express.json())
 app.use(cookieParser())
+app.use(
+  cors({
+    origin: [process.env.Frontend],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use("/api/v1/manufacturer",ManufacturerRouter)
 app.use("/api/v1/transporter",TransportersRouter)
+ 
 
 app.get("/",(req,res)=>{
     res.send("hello")
