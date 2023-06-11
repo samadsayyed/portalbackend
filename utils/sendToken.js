f0e19c7baa58bc411c780d2111d153f37540d33f
samadsayyed <samadsayyed812@gmail.com>
2023-06-11 12:16:36 +0530

    setting http

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -2,7 +2,10 @@ import jwt from "jsonwebtoken"
 
 export const sendToken = (res,user) =>{
     const token = jwt.sign({_id:user._id},process.env.Secret)
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.NODE_ENV === "Development");
     res.cookie("token",token,{
+        httpOnly:true,
         maxAge:process.env.Token_expire*1000*60*60*24,
         sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
       secure: process.env.NODE_ENV === "Development" ? false : true,
